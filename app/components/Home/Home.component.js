a026bff320706f35c5bd86b35bb6991e03cc218f
Anton Suhorada <fpscinema1@gmail.com>
2019-09-26 11:36:44 +0300

    Added some styling for views

--- app/components/Home/Home.component.js
+++ app/components/Home/Home.component.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { TextInput, View ,Text} from 'react-native';
-import PropTypes from 'prop-types';
 import styles from './Home.component.style';
 import TextArea from '../TextArea/TextArea.component';
 
 class Home extends Component{
+    state = {
+        title : ''
+    }
+    setTitle = (title) => this.setState({title})
     render(){
         return(
             <View style={styles.container}>
-                <Text>Please enter your note here</Text>
-                <TextArea />
+                <Text style={styles.titleHeading}> Note Title</Text>
+                <TextInput style={styles.titleTextInput}
+                    onChangeText={this.setTitle} value={this.state.title} />
+                <Text style={styles.textAreaTitle}> Please type your note below </Text>
+                <TextArea style={styles.textArea}/>
             </View>
         );
     }
